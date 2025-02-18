9251c8bfdd7e29a858ba8b9eb8a7542efb34af16
muhfaris <devmuhfaris@gmail.com>
2025-02-18 17:49:45 +0700

    fix(homework): bugs

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -13,17 +13,17 @@ contract BuggyContract {
     }
 
     // Bug 1
-    () {
+    constructor() {
         owner = msg.sender;
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    function increment(uint amount) public {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() public view returns (uint) {
         return count;
     }
 
@@ -35,7 +35,7 @@ contract BuggyContract {
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
+    function setMessage(string memory message) public {
         messages = message;
     }
 
@@ -43,12 +43,13 @@ contract BuggyContract {
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
-        uint sum;
-        for (uint i; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+    function sumNumbers(uint n) public pure returns (uint) {
+        // uint sum;
+        // for (uint i; i <= n; i++) {
+        //     sum += i;
+        // }
+        // return sum;
+        return (n * (n + 1)) / 2;
     }
 
     // Bug 7
@@ -58,12 +59,12 @@ contract BuggyContract {
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    function resetCount() private onlyOwner {
         count = 0;
     }
 
     // Bug 9
-    function togglePause() public onlyOwner {
+    function togglePause() private onlyOwner {
         isPaused = isPaused;
     }
 }
