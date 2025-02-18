0865666cc8e39de905d44e480d4dbd10a09a3503
GusdeRizky <148935891+GusdeRizky@users.noreply.github.com>
2025-02-18 18:46:32 +0800

    Update BuggyContract.sol

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -18,12 +18,12 @@ contract BuggyContract {
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    function increment(uint amount) private {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() public view returns (uint) {
         return count;
     }
 
@@ -35,7 +35,7 @@ contract BuggyContract {
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
+    function setMessage(string memory message) public {
         messages = message;
     }
 
@@ -43,12 +43,8 @@ contract BuggyContract {
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
-        uint sum;
-        for (uint i; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+    function sumNumbers(uint n) public pure returns (uint) {
+        return (n * (n + 1)) / 2;
     }
 
     // Bug 7
@@ -58,7 +54,7 @@ contract BuggyContract {
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    function resetCount() public onlyOwner {
         count = 0;
     }
 
