595acf6b070b7cc78b404b432524d8da8a7d2ef4
Dante4rt <rxmxdhxni@gmail.com>
2025-02-18 23:32:50 +0700

    Homework Day 1 – Muhammad Ramadhani

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -13,17 +13,17 @@ contract BuggyContract {
     }
 
     // Bug 1
-    () {
+    constructor() {
         owner = msg.sender;
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    function increment(uint amount) private {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() public view returns (uint) {
         return count;
     }
 
@@ -35,7 +35,7 @@ contract BuggyContract {
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
+    function setMessage(string memory message) public {
         messages = message;
     }
 
@@ -43,22 +43,26 @@ contract BuggyContract {
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
+    function sumNumbers(uint n) public pure returns (uint) {
         uint sum;
-        for (uint i; i <= n; i++) {
+
+        for (uint i = 0; i < n; i++) {
             sum += i;
         }
+
         return sum;
     }
 
     // Bug 7
     // Hint: Think about math operation overflow
-    function setCountWithMultiplication(uint x, uint y) public {
+    function setCountWithMultiplication(uint x, uint y) public returns (uint) {
         count = x * y;
+
+        return count;
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    function resetCount() external onlyOwner {
         count = 0;
     }
 
