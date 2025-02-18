94497c775ac99e83ccded590aec75c92f96a09c2
imadegautama <imadegautama@gmail.com>
2025-02-18 15:24:41 +0800

    fix : homework bug ✨

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -2,40 +2,42 @@
 pragma solidity ^0.8.13;
 
 contract BuggyContract {
-    uint private count = 0;
+    uint256 private count = 0;
     string messages;
     bool public isPaused;
     address owner;
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
     // Bug 1
-    () {
+    function setOwner() public {
         owner = msg.sender;
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    function increment(uint256 amount) private {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() public view returns (uint256) {
         return count;
     }
 
     // Bug 4
-    function divideCount(uint divisor) public returns (uint) {
+    function divideCount(uint256 divisor) public returns (uint256) {
+        require(divisor != 0, "Divisor cannot be zero");
         count = count / divisor;
+
         return count;
     }
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
+    function setMessage(string memory message) public {
         messages = message;
     }
 
@@ -43,27 +45,26 @@ contract BuggyContract {
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
-        uint sum;
-        for (uint i; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+    function sumNumbers(uint256 n) public pure returns (uint256) {
+        return (n * (n + 1)) / 2;
     }
 
     // Bug 7
     // Hint: Think about math operation overflow
-    function setCountWithMultiplication(uint x, uint y) public {
-        count = x * y;
+    function setCountWithMultiplication(uint256 x, uint256 y) public {
+        unchecked {
+            require(x == 0 || (x * y) / x == y, "Multiplication overflow");
+            count = x * y;
+        }
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    function resetCount() public onlyOwner {
         count = 0;
     }
 
     // Bug 9
     function togglePause() public onlyOwner {
-        isPaused = isPaused;
+        isPaused = !isPaused;
     }
 }
