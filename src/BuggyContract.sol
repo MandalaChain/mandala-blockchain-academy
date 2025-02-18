bfe65fbcc465a103041ac4b3ac74c1d54993a2f0
Razeena Sheena <agungwork187080107@gmail.com>
2025-02-18 20:38:21 +0800

    day2 mandala

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -7,35 +7,38 @@ contract BuggyContract {
     bool public isPaused;
     address owner;
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
     // Bug 1
-    () {
+    constructor() {
         owner = msg.sender;
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    function increment(uint amount) private {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() public view returns (uint) {
         return count;
     }
 
     // Bug 4
     function divideCount(uint divisor) public returns (uint) {
+        if(divisor==0){
+            revert("Divisor can't be zero.");
+        }
         count = count / divisor;
         return count;
     }
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
+    function setMessage(string memory message) public {
         messages = message;
     }
 
@@ -43,27 +46,26 @@ contract BuggyContract {
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
-        uint sum;
-        for (uint i; i <= n; i++) {
-            sum += i;
-        }
+    function sumNumbers(uint n) public pure returns (uint) {
+        require(n <= type(uint).max / (n + 1), "The N makes overflow please makes lower");
+        uint sum = (n * (n + 1)) / 2;
         return sum;
     }
 
     // Bug 7
     // Hint: Think about math operation overflow
     function setCountWithMultiplication(uint x, uint y) public {
+        require(x == 0 || (type(uint).max / x) >= y, "The N makes overflow please makes lower");
         count = x * y;
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    function resetCount() public onlyOwner {
         count = 0;
     }
 
     // Bug 9
     function togglePause() public onlyOwner {
-        isPaused = isPaused;
+        isPaused = !isPaused;
     }
 }
