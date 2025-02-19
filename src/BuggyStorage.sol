dda9006563c1c95362c265c2725b19efe7f16a0c
GusdeRizky <148935891+GusdeRizky@users.noreply.github.com>
2025-02-19 16:48:47 +0800

    Update BuggyStorage.sol

--- src/BuggyStorage.sol
+++ src/BuggyStorage.sol
@@ -21,36 +21,47 @@ contract BuggyStorage {
         // Task 4: Implement adding numbers to the array
     }
 
-    function addNumber(uint number) public {
-        // Task 4: Implement adding numbers to the array
-    }
-
     // Bug 2: Slice array correctly
     function sliceArray(uint start, uint end) public view returns (uint[] memory) {
-        return numbers[start:end];
+        uint256 len = numbers.length;
+
+        require(start <= end && end <= len, "invalid");
+
+        uint256[] memory result = new uint256[](end - start);
+        for(uint256 i; i < result.length; i++){
+            result[i] = numbers[start + i];
+        }
+
+        return result;
     }
 
     // Bug 3: Concatenate strings correctly
     function setMessage(string memory newMessage) public {
-        message += newMessage;
+        message = string.concat(message,newMessage);
     }
 
     // Bug 4: How do we update numbers with memory as params?
     function validMemoryUsage(uint[] memory input) public {
-        uint[] storage ref = input;
+        // uint[] storage ref = input;
         // Implement logics to update numbers here
+        for(uint256 i = 0; i < input.length; i++){
+            numbers.push(input[i]);
+        }
     }
 
     function getBalance(address user) public view returns (uint) {
+        return balances[user];
         // Task 5: Returning the balance for a specific user
     }
 
     // Bug 5: How to safely pop elements from the array?
     function removeLastNumber() public {
+        require(numbers.length > 0, "array already empty");
         numbers.pop();
     }
 
     function updateBalance(address user, uint amount) public {
+      balances[user] = amount;
         // Task 6: Update balance of a user with a specific amount
     }
 }
