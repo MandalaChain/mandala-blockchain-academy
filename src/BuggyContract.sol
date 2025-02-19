b6221adb7ef2226c8eb23c17dbe97f101728f3d4
dedyblinda2025 <khirayinnura02@gmail.com>
2025-02-19 11:18:45 +0800

    Homework Day 2 by Dedy Blinda

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract BuggyContract {
-    uint private count = 0;
+    uint256 private count = 0;
     string messages;
     bool public isPaused;
     address owner;
@@ -12,58 +12,58 @@ contract BuggyContract {
         _;
     }
 
-    // Bug 1
-    () {
+    // Bug 1 - done
+    function setOwner() public {
         owner = msg.sender;
     }
 
-    // Bug 2
-    private function increment(uint amount) {
+    // Bug 2 - done
+    function increment(uint256 amount) private {
         count += amount;
     }
 
-    // Bug 3
-    function getCount() view returns (uint) {
+    // Bug 3 - done
+    function getCount() public view returns (uint256) {
         return count;
     }
 
-    // Bug 4
-    function divideCount(uint divisor) public returns (uint) {
+    // Bug 4 - done
+    function divideCount(uint256 divisor) public returns (uint256) {
         count = count / divisor;
         return count;
     }
 
-    // Bug 5
+    // Bug 5 - done
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
+    function setMessage(string memory message) public {
         messages = message;
     }
 
-    // Bug 6
+    // Bug 6 - done
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
-        uint sum;
-        for (uint i; i <= n; i++) {
+    function sumNumbers(uint256 n) public pure returns (uint256) {
+        uint256 sum = 0;
+        for (uint256 i = 0; i <= n; i++) {
             sum += i;
         }
         return sum;
     }
 
-    // Bug 7
+    // Bug 7 - done
     // Hint: Think about math operation overflow
-    function setCountWithMultiplication(uint x, uint y) public {
-        count = x * y;
+    function setCountWithMultiplication(uint256 x, uint256 y) public {
+        unchecked { count = x * y; }
     }
 
-    // Bug 8
-    function resetCount() onlyOwner {
+    // Bug 8 - done
+    function resetCount() public onlyOwner {
         count = 0;
     }
 
-    // Bug 9
+    // Bug 9 - done
     function togglePause() public onlyOwner {
-        isPaused = isPaused;
+        isPaused = !isPaused;
     }
 }
