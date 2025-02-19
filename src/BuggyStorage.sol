83a9d6f23ba608a12d8663c02bb037abfe68809e
MantaYuana <gunamanta.yuana@gmail.com>
2025-02-19 21:34:14 +0800

    Fixed BuggyStorage.sol

--- src/BuggyStorage.sol
+++ src/BuggyStorage.sol
@@ -4,44 +4,57 @@ pragma solidity ^0.8.0;
 contract BuggyStorage {
     // DO NOT EDIT COMMENTS
     // Task 1: Declare an array as numbers;
+    uint256[] public numbers;
     // Task 2: Declare a string as message;
+    string public message;
     // Task 3: Declare a mapping of address to balance as balances;
+    mapping(address => uint) public balance;
 
     // Bug 1: Array bounds
     function getNumber(uint index) public view returns (uint) {
+        require(index < numbers.length, "Array out of bounds");
         return numbers[index]; 
     }
 
     function addNumber(uint number) public {
         // Task 4: Implement adding numbers to the array
+        numbers.push(number);
     }
 
     // Bug 2: Slice array correctly
     function sliceArray(uint start, uint end) public view returns (uint[] memory) {
-        return numbers[start:end];
+        uint256[] memory _sliceArray = new uint256[](end - start);
+        for (uint256 i = start; i < end; i++) {
+            _sliceArray[i - start] = numbers[i];
+        }
+        return _sliceArray;
     }
 
     // Bug 3: Concatenate strings correctly
     function setMessage(string memory newMessage) public {
-        message += newMessage;
+        message = string(abi.encodePacked(message, newMessage));
     }
 
     // Bug 4: How do we update numbers with memory as params?
     function validMemoryUsage(uint[] memory input) public {
-        uint[] storage ref = input;
+        uint[] memory ref = input;
         // Implement logics to update numbers here
+        numbers = ref;
     }
 
     function getBalance(address user) public view returns (uint) {
         // Task 5: Returning the balance for a specific user
+        return balance[user];
     }
 
     // Bug 5: How to safely pop elements from the array?
     function removeLastNumber() public {
+        require(numbers.length > 0, "Array must not be empty !");
         numbers.pop();
     }
 
     function updateBalance(address user, uint amount) public {
         // Task 6: Update balance of a user with a specific amount
+        balance[user] = amount;
     }
 }
