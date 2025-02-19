73998249eb71a3ef1578e581763abc27980dc9b2
Dandy-Adinata <dandyadinata9@gmail.com>
2025-02-19 22:24:21 +0800

    Perbaikan bug pada BuggyContract.sol

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -13,57 +13,58 @@ contract BuggyContract {
     }
 
     // Bug 1
-    () {
+    constructor() {
         owner = msg.sender;
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    private increment(uint amount) public {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() view public returns (uint) {
         return count;
     }
 
     // Bug 4
     function divideCount(uint divisor) public returns (uint) {
+        require(divisor !=0, "Divisor cannot be zero");
         count = count / divisor;
         return count;
     }
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
-        messages = message;
+    string public tempMessage;
+    function setMessage(string memory message) public {
+        tempMessages = message;
     }
 
     // Bug 6
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
-        uint sum;
-        for (uint i; i <= n; i++) {
-            sum += i;
+    function sumNumbers(uint n) public pure returns (uint) {
+        return (n * (n + 1)) / 2;
         }
-        return sum;
-    }
 
     // Bug 7
     // Hint: Think about math operation overflow
+    require(x == 0 || (x * y) / x == y, "Multiplication overflow");
     function setCountWithMultiplication(uint x, uint y) public {
         count = x * y;
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    uint public count;
+    function resetCount() public onlyOwner {
         count = 0;
     }
 
     // Bug 9
+    bool public isPaused;
     function togglePause() public onlyOwner {
-        isPaused = isPaused;
+        isPaused = !isPaused;
     }
 }
