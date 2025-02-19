e939cda17ba457a6b582b7b42497e00df45454c2
Wisnu <wardhana483@gmail.com>
2025-02-19 21:27:08 +0700

    optimize loop in BuggyContract.sol

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -3,39 +3,40 @@ pragma solidity ^0.8.13;
 
 contract BuggyContract {
     uint private count = 0;
-    string messages;
+    string public messages;
     bool public isPaused;
-    address owner;
+    address  public owner;
 
-    modifier onlyOwner {
+    modifier onlyOwner () {
         require(msg.sender == owner);
         _;
     }
 
     // Bug 1
-    () {
+    constructor () public {
         owner = msg.sender;
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    function increment(uint amount) public {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() public view returns (uint) {
         return count;
     }
 
     // Bug 4
     function divideCount(uint divisor) public returns (uint) {
+        require (divisor != 0 , "Cannot divide by zero");
         count = count / divisor;
         return count;
     }
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
+    function setMessage(string memory message) public {
         messages = message;
     }
 
@@ -50,16 +51,17 @@ contract BuggyContract {
     // Bug 7
     // Hint: Think about math operation overflow
     function setCountWithMultiplication(uint x, uint y) public {
+        require(x <= type(uint).max / y , "Overflow deteced");
         count = x * y;
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    function resetCount() public onlyOwner {
         count = 0;
     }
 
     // Bug 9
     function togglePause() public onlyOwner {
-        isPaused = isPaused;
+        isPaused = !isPaused;
     }
 }
