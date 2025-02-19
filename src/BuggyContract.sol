df3b9d17c099dc77bd98e1eb88e73d8da809e7b1
RXRD <lilixinxin@protonmail.com>
2025-02-19 14:23:12 +0700

    Update BuggyContract.sol

--- src/BuggyContract.sol
+++ src/BuggyContract.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+error DivisionByZero();
 
 contract BuggyContract {
     uint private count = 0;
@@ -13,29 +14,32 @@ contract BuggyContract {
     }
 
     // Bug 1
-    () {
+    constructor () {
         owner = msg.sender;
     }
 
     // Bug 2
-    private function increment(uint amount) {
+    function increment(uint amount) private {
         count += amount;
     }
 
     // Bug 3
-    function getCount() view returns (uint) {
+    function getCount() public view returns (uint) {
         return count;
     }
 
     // Bug 4
     function divideCount(uint divisor) public returns (uint) {
+        if (divisor == 0) 
+            revert DivisionByZero() ;
+        
         count = count / divisor;
         return count;
     }
 
     // Bug 5
     // Hint: make sure that message is only stored temporarily
-    function setMessage(string message) public {
+    function setMessage(string memory message) public {
         messages = message;
     }
 
@@ -43,27 +47,28 @@ contract BuggyContract {
     // Hint: two things to do here.
     // Think about how to make the loop more efficient
     // and read the compiler
-    function sumNumbers(uint n) public returns (uint) {
+    function sumNumbers(uint n) public pure returns (uint) {
         uint sum;
-        for (uint i; i <= n; i++) {
-            sum += i;
-        }
+        if ((n % 2) == 0) sum = (n + 1) * (n / 2);
+        if ((n % 2) != 0) sum = n * ((n+1)/2);
         return sum;
+
     }
 
     // Bug 7
     // Hint: Think about math operation overflow
     function setCountWithMultiplication(uint x, uint y) public {
+        require(x == 0 || ((x * y) / x) == y);
         count = x * y;
     }
 
     // Bug 8
-    function resetCount() onlyOwner {
+    function resetCount() private onlyOwner {
         count = 0;
     }
 
     // Bug 9
     function togglePause() public onlyOwner {
-        isPaused = isPaused;
+        isPaused = !isPaused;
     }
 }
