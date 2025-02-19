e1d3508fd3aa380a196b768750a849939d0f6db0
Irfansyah-Maker <irfanfaiz7221@gmail.com>
2025-02-20 01:29:07 +0800

    Day 3_Irfan

--- src/BuggyStorage.sol
+++ src/BuggyStorage.sol
@@ -6,42 +6,57 @@ contract BuggyStorage {
     // Task 1: Declare an array as numbers;
     // Task 2: Declare a string as message;
     // Task 3: Declare a mapping of address to balance as balances;
+    uint256[] public numbers;
+    string public message;
+    mapping(address => uint256) public balance;
 
     // Bug 1: Array bounds
     function getNumber(uint index) public view returns (uint) {
+        require (index < numbers.length, "array index out of bounds");
         return numbers[index]; 
     }
 
     function addNumber(uint number) public {
+        numbers.push(number);
         // Task 4: Implement adding numbers to the array
     }
 
     // Bug 2: Slice array correctly
     function sliceArray(uint start, uint end) public view returns (uint[] memory) {
-        return numbers[start:end];
+        require(start < end, "less than end index");
+        require(end <= numbers.length, "less than");
+        uint[] memory slicedArray = new uint[](end - start);
+        for(uint256 i = start; i< end; i++){
+            slicedArray[i - start] = numbers[i];
+        }
+        return slicedArray;
     }
 
     // Bug 3: Concatenate strings correctly
     function setMessage(string memory newMessage) public {
-        message += newMessage;
+        message = string(abi.encodePacked(message, newMessage));
     }
 
     // Bug 4: How do we update numbers with memory as params?
     function validMemoryUsage(uint[] memory input) public {
-        uint[] storage ref = input;
+        for (uint256 i = 0; i < input.length; i++) {
+            numbers.push(input[i]);
         // Implement logics to update numbers here
     }
-
+    }
     function getBalance(address user) public view returns (uint) {
+        return balance[user];
         // Task 5: Returning the balance for a specific user
     }
 
     // Bug 5: How to safely pop elements from the array?
     function removeLastNumber() public {
+        require(numbers.length > 0, "doesnt displayed");
         numbers.pop();
     }
 
     function updateBalance(address user, uint amount) public {
+        balance[user] = amount;
         // Task 6: Update balance of a user with a specific amount
     }
 }
